209ece2bdbbacb9791a8c8768b9eaf572719ac6c
Nikolay <npenchev19@gmail.com>
2021-10-28 11:48:12 +0300

    adding CRUD GUI admin panel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('index');
+});
+Route::prefix('admin')->name('admin.')->group(function (){
+    Route::resource('/users',UserController::class);
 });
